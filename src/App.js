353086282cdfc6fdc477db191104719b1dbb6671
Alexander <you@example.com>
2022-12-19 08:36:45 -0800

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react';
+import Navbar  from './Components/layout/Navbar';
+import './App.css';
+class App extends React.Component{
+  render() {
+
+
+return  (
+<div className='App'>
+<Navbar title = {5} />
+</div>
+)
+//React.createElement(
+// 'div', {className:"app" },
+// React.createElement('h1', null, "Hello From React")\
+
+// )
+//       <div className="App">
+//      <h1>Hello from React</h1>
+//      <h2>Goodbye
+//      </h2>
+//       </div>
+  
+  }
 }
+
 
 export default App;
